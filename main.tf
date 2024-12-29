8cf125c6c0c9a1cc19b87d47568a18e35ab95e5b
UcheHopeO <ucheodoabuchi@gmail.com>
2024-12-29 11:19:55 +0000

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraform-dec-project23" {
+  bucket = "terraform-dec-project23"
+  }
+  terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-dec-project23"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
